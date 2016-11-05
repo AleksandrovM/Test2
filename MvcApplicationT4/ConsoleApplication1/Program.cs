d302b92d75855c081e80e60a509dca7394c3873e
Matvey A <luxlama@yandex.ru>
2016-11-05 19:31:45 +0300

    play round

--- MvcApplicationT4/ConsoleApplication1/Program.cs
+++ MvcApplicationT4/ConsoleApplication1/Program.cs
@@ -8,7 +8,89 @@ using System.Threading;
 namespace ConsoleApplication1
 {
 
-     
+    public class EmployeeOptionEntry
+    {
+        public int id;
+        public long optionsCount;
+        public DateTime dateAwarded;
+
+        public static EmployeeOptionEntry[] GetEmployeeOptionEntries()
+        {
+            EmployeeOptionEntry[] empOptions = new EmployeeOptionEntry[] {
+                new EmployeeOptionEntry { 
+                  id = 1, 
+                  optionsCount = 2, 
+                  dateAwarded = DateTime.Parse("1999/12/31") },
+                new EmployeeOptionEntry { 
+                  id = 2, 
+                  optionsCount = 10000, 
+                  dateAwarded = DateTime.Parse("1992/06/30")  },
+                new EmployeeOptionEntry { 
+                  id = 2, 
+                  optionsCount = 10000, 
+                  dateAwarded = DateTime.Parse("1994/01/01")  },
+                new EmployeeOptionEntry { 
+                  id = 3, 
+                  optionsCount = 5000, 
+                  dateAwarded = DateTime.Parse("1997/09/30") },
+                new EmployeeOptionEntry { 
+                  id = 2, 
+                  optionsCount = 10000, 
+                  dateAwarded = DateTime.Parse("2003/04/01")  },
+                new EmployeeOptionEntry { 
+                  id = 3, 
+                  optionsCount = 7500, 
+                  dateAwarded = DateTime.Parse("1998/09/30") },
+                new EmployeeOptionEntry { 
+                  id = 3, 
+                  optionsCount = 7500, 
+                  dateAwarded = DateTime.Parse("1998/09/30") },
+                new EmployeeOptionEntry { 
+                  id = 4, 
+                  optionsCount = 1500, 
+                  dateAwarded = DateTime.Parse("1997/12/31") },
+                new EmployeeOptionEntry { 
+                  id = 101, 
+                  optionsCount = 2, 
+                  dateAwarded = DateTime.Parse("1998/12/31") }
+              };
+
+            return (empOptions);
+        }
+    }
+
+    public class optComparer : IEqualityComparer<int>
+    {
+
+        public bool Equals(int x, int y)
+        {
+            if (x < 100 && y  < 100)
+            {
+                return true;
+            }
+            else if (x > 100 && y > 100)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+            
+        }
+
+        public int GetHashCode(int i)
+        {
+            int f = 1;
+            int nf = 100;
+            return (isFounder(i) ? f.GetHashCode() : nf.GetHashCode());
+        }
+
+        public bool isFounder(int id)
+        {
+            return (id < 100);
+        }
+    }
+
     [Flags]
     public enum Days
     { 
@@ -31,33 +113,72 @@ namespace ConsoleApplication1
          
         static void Main(string[] args)
         {
-            Func<string> f = () => SomeMethod() ?? " Un ti t l ed ";
-            string aa = f();
+            //Func<string> f = () => SomeMethod() ?? " Un ti t l ed ";
+            //string aa = f();
 
-            Console.WriteLine(aa);
+            //Console.WriteLine(aa);
 
-            Days dd = Days.mon | Days.tu;
+            //Days dd = Days.mon | Days.tu;
 
-            int a = (int)dd;
+            //int a = (int)dd;
 
-            Days ff = Days.sa;
+            //Days ff = Days.sa;
 
-            if ((int)(dd & ff) != 0)
+            //if ((int)(dd & ff) != 0)
+            //{
+            //    Console.WriteLine('1');
+            //}
+            //else
+            //{
+            //    Console.WriteLine('0');
+            //}
+
+            var qqq = EmployeeOptionEntry.GetEmployeeOptionEntries();
+
+            var outPos = qqq.GroupBy(k => k.id);
+
+            foreach (var i in outPos)
             {
-                Console.WriteLine('1');
+                Console.WriteLine(" " + i.Key);
+
+                foreach (var j in i)
+                {
+                    Console.WriteLine("  "+ j.id + " " + j.optionsCount);
+                }
             }
-            else
+
+            Console.WriteLine();
+
+            var myC = new optComparer();
+
+            var out2 = qqq.GroupBy(k => k.id, myC);
+
+            foreach (var i in out2)
             {
-                Console.WriteLine('0');
+                Console.WriteLine(" " + i.Key);
+
+                foreach (var j in i)
+                {
+                    Console.WriteLine("  " + j.id + " " + j.optionsCount);
+                }
             }
 
+            Console.WriteLine();
 
+            var outPos3 = qqq.GroupBy(k => k.id, e => e.dateAwarded);
+
+            foreach (var i in outPos3)
+            {
+                Console.WriteLine(" " + i.Key);
+
+                foreach (var j in i)
+                {
+                    Console.WriteLine("date = " + j);
+                }
+            }
             
             Console.ReadLine();
-
-            int baa = Int32.MaxValue;
-
-            baa = baa + 2;
+             
         }
     }
 }
